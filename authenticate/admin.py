3c1379cca456e1ee5f35d75db52fad58fc05932e
AmirHossein_Enayati <eamirh19@outlook.com>
2022-10-06 17:14:14 +0330

    'Database_Designed_1'

--- authenticate/admin.py
+++ authenticate/admin.py
@@ -1,3 +1,7 @@
+from urllib.parse import uses_relative
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+#admin.site.register(Role, Medicine,Therapy_List,Therapy,Test,Test_Session,Note,Organization)
+models_register = [Role, Medicine,Therapy_List,Therapy,Test,Test_Session,Note,Organization,User]
+admin.site.register(models_register)
--- authenticate/models.py
+++ authenticate/models.py
@@ -1,3 +1,81 @@
+from pyexpat import model
 from django.db import models
+from django.contrib.auth.models import User ,AbstractBaseUser
+from django.db.models.fields.related import ForeignKey
+from django.db.models.fields.reverse_related import ManyToManyRel
 
-# Create your models here.
+
+class Medicine(models.Model):
+    medicineID = models.IntegerField(primary_key=True,null=False)
+    name = models.CharField(max_length=45,null = False)
+
+
+class Therapy_List(models.Model):
+    therapy_listID = models.IntegerField(primary_key=True,null=False)
+    name = models.CharField(max_length=45,null = False)
+
+
+    Medicine_IDmedicine = models.ForeignKey(
+    Medicine, on_delete=models.CASCADE, blank=True, null=False)
+
+class Therapy(models.Model):
+    therapyID = models.IntegerField(primary_key=True,null=False)
+
+    #ForigenKey
+    User_IDpatient = models.ForeignKey(
+    User, on_delete=models.CASCADE, blank=True, null=False)
+    TherapyList_IDtherapylist = models.ForeignKey(
+    Therapy_List, on_delete=models.CASCADE, blank=True, null=False)
+
+class Test(models.Model):
+    testID = models.IntegerField(primary_key=True,null=False)
+    dateTime = models.DateField(null=False)
+
+    #ForigenKey
+    Therapy_IDtherapy = models.ForeignKey(
+    Therapy, on_delete=models.CASCADE, blank=True, null=False)
+
+
+
+class Test_Session(models.Model):
+    test_SessionID = models.IntegerField(primary_key=True,null=False)
+    test_type = models.IntegerField(null=False)
+    DataURL = models.CharField(max_length=255,null = False)
+
+    #ForigenKey
+    Test_IDtest = models.ForeignKey(
+    Test, on_delete=models.CASCADE, blank=True, null=False)
+
+
+class Note(models.Model):
+    noteID = models.IntegerField(primary_key=True,null=False)
+    note = models.TextField()
+
+    #ForigenKey
+    Test_Session_IDtest_session = models.ForeignKey(
+    Test_Session, on_delete=models.CASCADE, blank=True, null=False)
+    User_IDmed = models.ForeignKey(
+    User, on_delete=models.CASCADE, blank=True, null=False)
+
+
+
+
+
+
+class Organization(models.Model):
+    organizationID = models.IntegerField(primary_key=True,null=False)
+    name = models.CharField(max_length=255,null = False)
+
+class Role(models.Model):
+    roleID = models.IntegerField(primary_key=True,null=False)
+    name = models.CharField(max_length=45,null = False)
+    type = models.CharField(max_length=45,null = False)
+
+
+class User(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    userID = models.IntegerField(primary_key=True,null=False)
+    Role_IDrole = models.ForeignKey(
+    Role, on_delete=models.CASCADE, blank=True, null=False)
+    Organization = models.ForeignKey(
+    Organization, on_delete=models.CASCADE, blank=True, null=False)
